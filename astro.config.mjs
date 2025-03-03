accfa01c3d759cbaf84d5cafd5d4493173158266
shinichi kogiso <kuusai1998@gmail.com>
2025-03-03 17:24:33 +0900

    Google Analyticsを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,18 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [
+    icon(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
